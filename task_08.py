5961d1002c8d680bd8da27f0ff6587ea2cd6ed50
tatyana86 <tanyaridman@gmail.com>
2026-05-04 22:28:47 +0400

    add task_08

---
+++ task_08.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+def odometer(oksana):
+    pairs = list(zip(oksana[::2], oksana[1::2]))
+    speeds, times = zip(*pairs)
+    deltas = (t - p for p, t in zip((0, *times[:-1]), times))
+    return sum(map(lambda s, dt: s * dt, speeds, deltas))
+	
+print(odometer([15, 1, 25, 2, 30, 3, 10, 5]))
